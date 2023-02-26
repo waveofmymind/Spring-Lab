ffcc5dbaec5189f1d7b2efdbc526e077a5f2a0e7
waveofmymind <sjun0913@gmail.com>
2023-02-26 13:14:07 +0900

    [feat] 객체 지향적으로 User 메서드 내에서 PasswordValidator를 생성하기 보다 파라미터로 주입받아 사용해서 의존성을 낮추기

--- oop-practice/src/main/java/org/example/RandomPasswordGenerator.java
+++ oop-practice/src/main/java/org/example/RandomPasswordGenerator.java
@@ -5,7 +5,7 @@ import org.passay.CharacterRule;
 import org.passay.EnglishCharacterData;
 import org.passay.PasswordGenerator;
 
-public class RandomPasswordGenerator {
+public class RandomPasswordGenerator implements org.example.PasswordGenerator {
 
     public static final String ALLOWED_SPL_CHARACTERS = "!@#$%^&*()_+";
 
