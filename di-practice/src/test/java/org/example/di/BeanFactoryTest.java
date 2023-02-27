476b46943c8a9b4abc4fb380e51652f792830323
waveofmymind <sjun0913@gmail.com>
2023-02-27 18:41:02 +0900

    [feat] BeanFactory를 통한 DI 테스트 완료

---
+++ di-practice/src/test/java/org/example/di/BeanFactoryTest.java
@@ -0,0 +1,45 @@
+package org.example.di;
+
+import org.assertj.core.api.Assertions;
+import org.example.annotation.Controller;
+import org.example.annotation.Service;
+import org.example.controller.UserController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeanFactoryTest {
+
+    private Reflections reflections;
+    private BeanFactory beanFactory;
+
+    @BeforeEach
+    void setUp() {
+         reflections = new Reflections("org.example");
+         // UserController,UserService
+         Set<Class<?>> preInstantiatedClazz = getTypesAnnotatedWith(Controller.class, Service.class);
+         beanFactory = new BeanFactory(preInstantiatedClazz);
+    }
+
+    private Set<Class<?>> getTypesAnnotatedWith(Class<? extends Annotation> ... annotations) {
+        Set<Class<?>> beans = new HashSet<>();
+        for (Class<? extends Annotation> annotation:annotations) {
+            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
+        }
+        return beans;
+    }
+
+    @Test
+    void diTest() {
+        UserController userController = beanFactory.getBean(UserController.class);
+        assertThat(userController).isNotNull();
+        assertThat(userController.getUserService()).isNotNull();
+    }
+}
