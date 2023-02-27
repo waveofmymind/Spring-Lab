eccc648715475f8b81f64be2af95b5780e599723
waveofmymind <sjun0913@gmail.com>
2023-02-27 09:08:10 +0900

    [TODO] DispatcherServlet 매핑 안되는 문제 해결하기

--- mvc-practice/src/main/java/org/example/WebApplicationServer.java
+++ mvc-practice/src/main/java/org/example/WebApplicationServer.java
@@ -8,15 +8,15 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 
 public class WebApplicationServer {
+    private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
 
-    public static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
-    public static void main(String[] args) throws LifecycleException {
-        String webappDirLocation="webapps/";
+    public static void main(String[] args) throws Exception {
+        String webappDirLocation = "webapps/";
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
 
         tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
-        log.info("configuring app with basedir: {}", new File(webappDirLocation).getAbsolutePath());
+        log.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
 
         tomcat.start();
         tomcat.getServer().await();
---
+++ mvc-practice/webapps/home.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="kr">
+<head>
+    <meta charset="UTF-8">
+    <title>home</title>
+</head>
+<body>
+Home 페이지
+</body>
+</html>
